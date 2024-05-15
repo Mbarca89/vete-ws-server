307288374e470167b590983d640c0740f05565e6
Mbarca89 <mauriciobarca1989@gmail.com>
2024-05-15 07:32:09 -0300

    message

--- src/controllers/sendMessage.js
+++ src/controllers/sendMessage.js
@@ -1,18 +1,17 @@
 const client = require("../client/client")
 
 const sendWaMessage = async (req, res) => {
-    const numberRegex = /^549\d{9}$/;
+    const numberRegex = /^549\d{10}$/;
 
     try {
         const { number, message } = req.body
         if (!number) throw Error("Ingrese el numero del destinatario")
-        if (!number.match(argentinaPhoneNumberRegex)) throw Error ("El número es incorrecto")
+        if (!number.match(numberRegex)) throw Error("El número es incorrecto")
         if (!message) throw Error("Ingrese el mensaje")
-
         const response = await client.sendMessage(`${number}@c.us`, message)
-        return res.status(200).json({message: "mensaje enviado"})
+        return res.status(200).json({ message: "mensaje enviado" })
     } catch (error) {
-        return res.status(400).json({message:`Faltan datos: ${error.message}`})
+        return res.status(400).json({ message: `Faltan datos: ${error.message}` })
     }
 }
 

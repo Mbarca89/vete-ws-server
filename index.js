3ebfa1f95b9ccd3e45b882ef0852c88d1c517236
Mbarca89 <mauriciobarca1989@gmail.com>
2024-05-16 20:37:03 -0300

    ready

--- index.js
+++ index.js
@@ -19,43 +19,43 @@ let generatedQR = null;
 let clientInitialized = false;
 
 client.on('qr', async (qr) => {
-    console.log('QR RECEIVED', qr);
-    try {
-      const qrDataURL = await QRCode.toDataURL(qr);
-      io.emit('qr', qrDataURL);
-      generatedQR = qrDataURL;
+  console.log('QR RECEIVED', qr);
+  try {
+    const qrDataURL = await QRCode.toDataURL(qr);
+    io.emit('qr', qrDataURL);
+    generatedQR = qrDataURL;
   } catch (error) {
-      console.error('Error generating QR code:', error);
+    console.error('Error generating QR code:', error);
   }
 });
 
 client.on('ready', async () => {
-    console.log('Client is ready!');
-    clientInitialized = true;
-    io.emit('ready', 'WhatsApp web esta conectado!');
+  console.log('Client is ready!');
+  clientInitialized = true;
+  io.emit('ready', 'WhatsApp web esta conectado!');
 });
 
 client.on('disconnected', (reason) => {
-    console.log('client disconnected:', reason);
-    io.emit('logedOut', 'WhatsApp web esta desconectado!');
+  console.log('client disconnected:', reason);
+  io.emit('logedOut', 'WhatsApp web esta desconectado!');
 });
 
 client.initialize();
 
 io.on('connection', async (socket) => {
-    console.log('Client connected');
-    if(clientInitialized) {
-      const clientStatus = await client.getState()
-      console.log(clientStatus)
-      if(clientStatus == "CONNECTED") io.emit('ready', 'WhatsApp web esta conectado!');
-    }
-    io.emit('serverReady', 'Server Started')
-    if (generatedQR) {
-        socket.emit('qr', generatedQR);
-    }
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');       
-    });
+  console.log('Client connected');
+  if (clientInitialized) {
+    const clientStatus = await client.getState()
+    console.log(clientStatus)
+    if (clientStatus == "CONNECTED") io.emit('ready', 'WhatsApp web esta conectado!');
+  }
+  io.emit('serverReady', 'Server Started')
+  if (generatedQR) {
+    socket.emit('qr', generatedQR);
+  }
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -65,5 +65,5 @@ app.use('/', routes);
 app.use(cors())
 
 server.listen(3001, () => {
-    console.log('Server is running on port 3001');
+  console.log('Server is running on port 3001');
 });
